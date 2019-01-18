3cd57576c787559253fab45011e1d8cae138f85a
maxiaochao <maxiaochao@jiekecloud.com>
2019-01-18 14:16:36 +0800

    Modify CacheManager func.

--- src/test/java/com/geek/commons/cache/CacheManagerTest.java
+++ src/test/java/com/geek/commons/cache/CacheManagerTest.java
@@ -1,8 +1,10 @@
 package com.geek.commons.cache;
 
+import com.geek.commons.cache.enums.CacheType;
 import com.geek.commons.cache.impl.ConcurrentHashMapCache;
 import com.geek.commons.cache.impl.HashMapCache;
 import org.junit.Test;
+import org.springframework.data.redis.core.RedisTemplate;
 
 public class CacheManagerTest {
 
@@ -10,4 +12,18 @@ public class CacheManagerTest {
     public void createComposeCache() {
         CacheManager.createComposeCache("id",new ConcurrentHashMapCache("id"),new HashMapCache("id"));
     }
+
+    @Test
+    public void createLocalCache() {
+        CacheManager.createLocalCache("id", CacheType.GUAVA);
+
+    }
+
+    @Test
+    public void createRedisCache() {
+        RedisTemplate redisTemplate=new RedisTemplate();
+        CacheManager.createRedisCache("id",redisTemplate);
+    }
+
+
 }
